ee448ed48e09bf6cf198a20b9e316f8c8123c834
Jan <linksunten@posteo.de>
2024-09-29 20:39:55 +0200

    Startseite einstellbar

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ToDo from './components/To-Do'; 
@@ -7,47 +7,73 @@ import Finanzen from './components/Finanzen';
 import Einstellungen from './components/Einstellungen'; 
 import { Ionicons } from '@expo/vector-icons'; 
 import { StatusBar } from 'expo-status-bar'; 
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [initialRoute, setInitialRoute] = useState(null);  // Standard auf null gesetzt, um Ladezustand zu erkennen
+  const [isLoading, setIsLoading] = useState(true);  // Ladezustand
+
+  useEffect(() => {
+    const getStartseite = async () => {
+      try {
+        const savedRoute = await AsyncStorage.getItem('startseite');
+        if (savedRoute) {
+          setInitialRoute(savedRoute);  // Gespeicherte Startseite verwenden
+        } else {
+          setInitialRoute('Tagebuch');  // Fallback zur Standard-Startseite
+        }
+      } catch (error) {
+        console.error('Error loading start page', error);
+        setInitialRoute('Tagebuch');  // Fallback bei Fehler
+      } finally {
+        setIsLoading(false);  // Ladezustand beenden
+      }
+    };
+
+    getStartseite();
+  }, []);
+
+  if (isLoading || initialRoute === null) {
+    return null;  // Hier kann ein Lade-Spinner oder eine andere Komponente hinzugefügt werden
+  }
+
   return (
     <>
       <StatusBar style="light" />
-        <NavigationContainer>
-          <Tab.Navigator
-            initialRouteName="Tagebuch"
-            screenOptions={({ route }) => ({
-              tabBarShowLabel: false,
-              tabBarIcon: ({ color, size }) => {
-                let iconName;
-                if (route.name === 'To-Do') {
-                  iconName = 'checkmark-done-outline'; 
-                } else if (route.name === 'Tagebuch') {
-                  iconName = 'book-outline'; 
-                } else if (route.name === 'Finanzen') {
-                  iconName = 'bar-chart-outline'; 
-                } else if (route.name === 'Einstellungen') {
-                  iconName = 'settings-outline'; 
-                }
-
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-              headerShown: false, 
-              tabBarStyle: {
-                backgroundColor: '#311b6b',  // Hier wird der Hintergrund des Bottom-Menüs gesetzt
-              },
-              tabBarActiveTintColor: 'tomato',
-              tabBarInactiveTintColor: '#e4d0ff',
-            })}
-          >
-            <Tab.Screen name="To-Do" component={ToDo} />
-            <Tab.Screen name="Tagebuch" component={Tagebuch} />
-            <Tab.Screen name="Finanzen" component={Finanzen} />
-            <Tab.Screen name="Einstellungen" component={Einstellungen} />
-
-          </Tab.Navigator>
-        </NavigationContainer>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName={initialRoute}  // Die initiale Route basierend auf AsyncStorage
+          screenOptions={({ route }) => ({
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+              if (route.name === 'To-Do') {
+                iconName = 'checkmark-done-outline'; 
+              } else if (route.name === 'Tagebuch') {
+                iconName = 'book-outline'; 
+              } else if (route.name === 'Finanzen') {
+                iconName = 'bar-chart-outline'; 
+              } else if (route.name === 'Einstellungen') {
+                iconName = 'settings-outline'; 
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            headerShown: false, 
+            tabBarStyle: {
+              backgroundColor: '#311b6b',
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: '#e4d0ff',
+          })}
+        >
+          <Tab.Screen name="To-Do" component={ToDo} />
+          <Tab.Screen name="Tagebuch" component={Tagebuch} />
+          <Tab.Screen name="Finanzen" component={Finanzen} />
+          <Tab.Screen name="Einstellungen" component={Einstellungen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </>
   );
 }
--- components/Einstellungen.js
+++ components/Einstellungen.js
@@ -1,14 +1,61 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import Checkbox from 'expo-checkbox';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Einstellungen() {
+    const [selectedStartseite, setSelectedStartseite] = useState('Tagebuch');  // Standard auf 'Tagebuch'
+
+    useEffect(() => {
+        const getStartseite = async () => {
+            const savedStartseite = await AsyncStorage.getItem('startseite');
+            if (savedStartseite) {
+                setSelectedStartseite(savedStartseite);
+            }
+        };
+        getStartseite();
+    }, []);
+
+    const saveStartseite = async (startseite) => {
+        setSelectedStartseite(startseite);
+        await AsyncStorage.setItem('startseite', startseite);  // Speichere die Auswahl
+    };
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Einstellungen</Text>
-            <Text style={styles.text}>Hier können Sie Einstellungen vornehmen.</Text>
-            <Text style={styles.text}></Text>
-            <Text style={styles.text}>Startseite</Text>
-            <Text style={styles.text}>Theme</Text>
+            <View style={styles.settingView}>
+                <Text style={styles.text}>Startseite</Text>
+                <Pressable onPress={() => saveStartseite('To-Do')}>
+                    <View style={styles.option}>
+                        <Checkbox
+                            style={styles.checkbox}
+                            value={selectedStartseite === 'To-Do'}
+                            onValueChange={() => saveStartseite('To-Do')}
+                        />
+                        <Text style={styles.paragraph}>To-Do-Liste</Text>
+                    </View>
+                </Pressable>
+                <Pressable onPress={() => saveStartseite('Tagebuch')}>
+                    <View style={styles.option}>
+                        <Checkbox
+                            style={styles.checkbox}
+                            value={selectedStartseite === 'Tagebuch'}
+                            onValueChange={() => saveStartseite('Tagebuch')}
+                        />
+                        <Text style={styles.paragraph}>Tagebuch</Text>
+                    </View>
+                </Pressable>
+                <Pressable onPress={() => saveStartseite('Finanzen')}>
+                    <View style={styles.option}>
+                        <Checkbox
+                            style={styles.checkbox}
+                            value={selectedStartseite === 'Finanzen'}
+                            onValueChange={() => saveStartseite('Finanzen')}
+                        />
+                        <Text style={styles.paragraph}>Finanzen</Text>
+                    </View>
+                </Pressable>
+            </View>
         </View>
     );
 }
@@ -17,14 +64,38 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#311b6b',
     },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 20,
+    settingView: {
+        margin: 8,
+        padding: 8,
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        borderRadius: 6,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+    },
+    option: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    checkbox: {
+        marginRight: 10,
+    },
+    paragraph: {
+        fontSize: 16,
     },
     text: {
         fontSize: 18,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        borderRadius: 6,
+        color: '#1e085a',
+        marginBottom: 8,
+        alignItems: 'center',
     },
 });
